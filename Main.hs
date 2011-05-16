b55c248a1398891e94456b083bdeacc1ea22454f
Tom Hawkins <tomahawkins@gmail.com>
2011-05-16 15:01:17 -0500

    updates

--- Main.hs
+++ Main.hs
@@ -3,30 +3,23 @@ module Main (main) where
 import qualified Data.ByteString as B
 
 import Eaton
-import MTL ()
+import MTL
 import PPC
 import Utils
 
 main :: IO ()
 main = do
-  B.writeFile "test.esb" $ B.pack $ esb $ bootloader $ assemble test
-  writeFile "test.sr" $ srec 0x00080000 0x0008000c $ bootloader $ assemble test
+  B.writeFile "test.esb" $ B.pack $ esb $            bootloader $ assemble $ compile test
+  writeFile "test.sr" $ srec 0x00080000 0x0008000c $ bootloader $ assemble $ compile test
 
-test :: PPC
-test = foldl1 (+++)
-  [ set R1 0xfffc0080  -- Base address of CAN A, buffer 0.
-  , set R2 0x8
-  , stb R2 0x0 R1      -- Write Code = 8.
-  , set R2 0x1
-  , stw R2 0x4 R1      -- Id = 1.
-  , set R2 0x01234567
-  , stw R2 0x8 R1
-  , set R2 0x89abcdef
-  , stw R2 0xc R1
-  , set R2 0x28
-  , stb R2 0x1 R1
-  , set R2 0xc
-  , stb R2 0x0 R1
-  , jumpToBoot
+-- A little MTL program that attempts to send a CAN message.
+test :: S
+test = Seq
+  [ 0xfffc0080 + 0x0 := 0x08000000  -- Write code 8 to CS buffer register.
+  , 0xfffc0080 + 0x4 := 0x00000001  -- Write dummy id.
+  , 0xfffc0080 + 0x8 := 0x01234587  -- Write dummy payload.
+  , 0xfffc0080 + 0xc := 0x89abcdef
+  , 0xfffc0080 + 0x0 := 0x082c0000  -- Write code c to CS register.  XXX What should be SRR and RTR?
+  , Asm $ assemble jumpToBoot
   ]
 
